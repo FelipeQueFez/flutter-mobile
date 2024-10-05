a025b177277175cbd4e5235dd406c5fae52f0a2f
“FelipeQueFez“ <felipequefez@gmail.com>
2024-10-05 16:31:55 +0100

    chore:basic widgets

--- lib/main.dart
+++ lib/main.dart
@@ -30,16 +30,136 @@ class ImageGallery extends StatelessWidget {
       appBar: AppBar(
         title: Text("Titulo da minha pagina"),
       ),
-      body: GridView.builder(
-        itemCount: images.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // Número de colunas
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.blue,
+              child: Text(
+                'Hello Flutter',
+                style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Text('Hello world'),
+            TextField(
+              decoration: InputDecoration(labelText: 'Username'),
+            ),
+            Column(
+              children: [
+                Text('item 1'),
+                Text('item 2'),
+                Text('item 3'),
+              ],
+            ),
+            Row(
+              children: [
+                Text('item 1'),
+                Text('item 2'),
+                Text('item 3'),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('TOCOU NO BOTAO');
+              },
+              child: Text('Incrementar'),
+            ),
+            Icon(
+              Icons.star,
+              color: Colors.blue,
+              size: 30,
+            ),
+            Card(
+              child: ListTile(
+                title: Text('titulo'),
+                subtitle: Text('subtitulo'),
+                leading: Icon(Icons.info),
+                trailing: Icon(Icons.arrow_forward),
+              ),
+            ),
+            DropdownButton<String>(
+              items: ['Option 1', 'Option 2', 'Option 3'].map((value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (value) {},
+              value: 'Option 1',
+            ),
+            Switch(value: false, onChanged: (value) {}),
+            Checkbox(value: false, onChanged: (value) {}),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.red,
+                    width: 200,
+                    height: 50,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.blue,
+                    width: 200,
+                    height: 50,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              width: 100,
+              height: 20,
+              child: Text(
+                'HELLO WORLD COM SIZEDBOX',
+                style: TextStyle(color: Colors.green),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.red,
+                    width: 200,
+                    height: 50,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.blue,
+                    width: 200,
+                    height: 50,
+                  ),
+                ),
+              ],
+            ),
+            GridView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: images.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // Número de colunas
+              ),
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Image.asset(images[index]),
+                );
+              },
+            ),
+            ListView(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              children: <Widget>[
+                ListTile(title: Text('item1')),
+                ListTile(title: Text('item2')),
+                ListTile(title: Text('item3')),
+              ],
+            ),
+          ],
         ),
-        itemBuilder: (context, index) {
-          return Card(
-            child: Image.asset(images[index]),
-          );
-        },
       ),
     );
   }
