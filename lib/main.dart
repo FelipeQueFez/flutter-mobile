0c80ac83dedd0bc0a3bd87f24864c2e7b2a9b532
“FelipeQueFez“ <felipequefez@gmail.com>
2024-10-05 17:36:17 +0100

    chore:checkpoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,165 +1,87 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MinhaPrimeiraTela());
+  runApp(MyApp());
 }
 
-class MinhaPrimeiraTela extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Image gallery app',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CounterStateless(),
+            SizedBox(
+              height: 20,
+            ),
+            CounterStateful(),
+          ],
+        ),
       ),
-      home: ImageGallery(),
     );
   }
 }
 
-class ImageGallery extends StatelessWidget {
-  final List<String> images = [
-    'assets/images/image1.jpg',
-    'assets/images/image2.jpg',
-    'assets/images/image3.png',
-  ];
+class CounterStateless extends StatelessWidget {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    _counter++;
+  }
 
   @override
-  Widget build(Object context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Titulo da minha pagina"),
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text('StatelessWidget'),
+          Text(
+            'Contagem:',
+            style: TextStyle(fontSize: 26),
+          ),
+          ElevatedButton(
+            onPressed: _incrementCounter,
+            child: Text('Incrementar'),
+          ),
+        ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              color: Colors.blue,
-              child: Text(
-                'Hello Flutter',
-                style: TextStyle(
-                  fontSize: 26,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            Text('Hello world'),
-            TextField(
-              decoration: InputDecoration(labelText: 'Username'),
-            ),
-            Column(
-              children: [
-                Text('item 1'),
-                Text('item 2'),
-                Text('item 3'),
-              ],
-            ),
-            Row(
-              children: [
-                Text('item 1'),
-                Text('item 2'),
-                Text('item 3'),
-              ],
-            ),
-            ElevatedButton(
-              onPressed: () {
-                print('TOCOU NO BOTAO');
-              },
-              child: Text('Incrementar'),
-            ),
-            Icon(
-              Icons.star,
-              color: Colors.blue,
-              size: 30,
-            ),
-            Card(
-              child: ListTile(
-                title: Text('titulo'),
-                subtitle: Text('subtitulo'),
-                leading: Icon(Icons.info),
-                trailing: Icon(Icons.arrow_forward),
-              ),
-            ),
-            DropdownButton<String>(
-              items: ['Option 1', 'Option 2', 'Option 3'].map((value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (value) {},
-              value: 'Option 1',
-            ),
-            Switch(value: false, onChanged: (value) {}),
-            Checkbox(value: false, onChanged: (value) {}),
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    color: Colors.red,
-                    width: 200,
-                    height: 50,
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    color: Colors.blue,
-                    width: 200,
-                    height: 50,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              width: 100,
-              height: 20,
-              child: Text(
-                'HELLO WORLD COM SIZEDBOX',
-                style: TextStyle(color: Colors.green),
-              ),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    color: Colors.red,
-                    width: 200,
-                    height: 50,
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    color: Colors.blue,
-                    width: 200,
-                    height: 50,
-                  ),
-                ),
-              ],
-            ),
-            GridView.builder(
-              physics: NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: images.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2, // Número de colunas
-              ),
-              itemBuilder: (context, index) {
-                return Card(
-                  child: Image.asset(images[index]),
-                );
-              },
-            ),
-            ListView(
-              physics: NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              children: <Widget>[
-                ListTile(title: Text('item1')),
-                ListTile(title: Text('item2')),
-                ListTile(title: Text('item3')),
-              ],
-            ),
-          ],
-        ),
+    );
+  }
+}
+
+class CounterStateful extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CounterStateful();
+  }
+}
+
+class _CounterStateful extends State<CounterStateful> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text('StatefulWidget'),
+          Text(
+            'Contagem: $_counter',
+            style: TextStyle(fontSize: 26),
+          ),
+          ElevatedButton(
+            onPressed: _incrementCounter,
+            child: Text('Incrementar'),
+          ),
+        ],
       ),
     );
   }
